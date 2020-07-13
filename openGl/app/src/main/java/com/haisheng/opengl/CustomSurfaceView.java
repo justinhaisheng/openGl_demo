b7609469725b2170d1f2f691d2d094dd519a8b9b
qiaoStr <236685708@qq.com>
2020-07-13 17:08:52 +0800

    simple demo

---
+++ openGl/app/src/main/java/com/haisheng/opengl/CustomSurfaceView.java
@@ -0,0 +1,22 @@
+package com.haisheng.opengl;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * @创建者 luhaisheng
+ * @创建时间 2020/7/13 16:58
+ * @描述
+ */
+public class CustomSurfaceView extends GLSurfaceView {
+    public CustomSurfaceView(Context context) {
+        this(context,null);
+    }
+
+    public CustomSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setEGLContextClientVersion(2);
+        setRenderer(new CustomRender());
+    }
+}
